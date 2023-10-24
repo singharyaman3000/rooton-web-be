5a47d5a696fa34af87e02cf93f1c225a23bd88f0
Aryaman Singh <aryaman.singh@rooton.ca>
2023-10-24 12:30:18 +0530

    Added All The Required Collections for CoachingPage & CoachingServices; linked with relevant entries.

---
+++ src/api/coaching-service/controllers/coaching-service.ts
@@ -0,0 +1,7 @@
+/**
+ * coaching-service controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::coaching-service.coaching-service');
---
+++ src/api/coaching-service/services/coaching-service.ts
@@ -0,0 +1,7 @@
+/**
+ * coaching-service service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::coaching-service.coaching-service');
