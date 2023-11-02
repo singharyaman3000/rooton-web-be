5c20b0dded019d1ffe2589eafb3e5bda0f7195e3
Anil Kuttappan <anil.kuttappan@qburst.com>
2023-11-02 12:51:20 +0530

    Updated the field type for aboutus stats

--- src/api/about-us-stat/controllers/about-us-stat.ts
+++
@@ -1,7 +0,0 @@
-/**
- * about-us-stat controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::about-us-stat.about-us-stat');
--- src/api/about-us-stat/services/about-us-stat.ts
+++
@@ -1,7 +0,0 @@
-/**
- * about-us-stat service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::about-us-stat.about-us-stat');
