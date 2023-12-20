60eddfe7ef5deb47dc83390e4d2b7e2526ca0737
mithunnqb <mithunn@qburst.com>
2023-12-20 14:43:17 +0530

    Update BeforeInstall.sh

--- BeforeInstall.sh
+++ BeforeInstall.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 cp /home/deploy/codedeploy/be/.env /home/deploy/codedeploy/be/v1/
-chown -R deploy.deploy /home/deploy/codedeploy/be/v1/
+sudo chown -R deploy.deploy /home/deploy/codedeploy/be/v1/
