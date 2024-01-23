2a09e2793d778e2e573e2087d45576abfcb4dc31
Aryaman Singh <aryaman.singh@rooton.ca>
2024-01-23 17:03:26 +0530

    Added Tools Schema
    Added 4 New Collections.
    
    1. ToolsHomePage
    2. ToolsHomePageContent
    3. Tools
    4. ToolsContent

---
+++ src/api/tools-content/controllers/tools-content.ts
@@ -0,0 +1,7 @@
+/**
+ * tools-content controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::tools-content.tools-content');
---
+++ src/api/tools-content/services/tools-content.ts
@@ -0,0 +1,7 @@
+/**
+ * tools-content service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::tools-content.tools-content');
